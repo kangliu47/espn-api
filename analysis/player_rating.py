3c5feacf057ce4a18e5f063cabf18bac36463a75
Liu, Kang <Kang.Liu@wolterskluwer.com>
2020-12-16 22:41:53 -0500

    add new functions for find similar players and calculate salary

--- analysis/player_rating.py
+++ analysis/player_rating.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from .utils.constants import stats_counts, shooting_counts
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity, euclidean_distances
 
 
 def transform_column(one_stats: pd.Series) -> pd.Series:
@@ -67,3 +69,66 @@ def add_overall_punt_one_category(
 
 def punt_column_name(punt_cat: str) -> str:
     return f"PUNT_{punt_cat}"
+
+
+def get_similar_players(
+    name: str,
+    player_ratings: pd.DataFrame,
+    score_method: str = "euclidean",
+    feature_cols: list = ["PTS", "BLK", "STL", "AST", "REB", "3PTM", "FGR", "FTR"],
+    n_players: int = 10,
+) -> pd.DataFrame:
+
+    function_and_sorting = {
+        "cosine": (cosine_similarity, False),
+        "euclidean": (euclidean_distances, True),
+    }
+    score_function, sort_ascending = function_and_sorting[score_method]
+    one_player = player_ratings.loc[name, feature_cols].to_frame().T
+    all_scores = score_function(X=one_player, Y=player_ratings[feature_cols])
+    similar_players = player_ratings.copy()
+    similar_players[score_method] = all_scores.reshape(-1, 1)
+    return similar_players.sort_values(by=score_method, ascending=sort_ascending).head(
+        n_players
+    )
+
+
+def get_expected_salary(
+    player_ratings: pd.DataFrame,
+    players_per_team: int = 13,
+    n_teams: int = 13,
+    one_dollar_rank: int = None,
+):
+    #! This method would not work for player_ratings calculated with only relevant players
+    relevant_players = players_per_team * n_teams
+
+    if one_dollar_rank is None:
+        one_dollar_rank = relevant_players
+
+    assert one_dollar_rank <= relevant_players
+    num_one_dollar_guys = relevant_players - one_dollar_rank
+    total_rating = player_ratings.head(one_dollar_rank)["overall"].sum()
+    salary_factor = (n_teams * 200 - num_one_dollar_guys) / total_rating
+    more_than_one_dollar = (
+        salary_factor * player_ratings["overall"].iloc[:one_dollar_rank]
+    )
+    one_dollar_players = pd.Series(
+        np.ones(shape=(player_ratings.iloc[one_dollar_rank:].shape[0])),
+        index=player_ratings.index[one_dollar_rank:],
+    )
+    salary_expectation = pd.concat([more_than_one_dollar, one_dollar_players])
+    return salary_expectation
+
+
+def combine_player_data(
+    data_2020: pd.DataFrame, data_2021: pd.DataFrame
+) -> pd.DataFrame:
+    missing_players = list(set(data_2020.index) - set(data_2021.index))
+    data_2021["stats_type"] = "2021_projection"
+    fill_with_2020 = data_2020.loc[missing_players]
+    fill_with_2020["stats_type"] = "2020_stats"
+    combined_data = pd.concat([data_2021, fill_with_2020])
+    if "overall" in combined_data.columns:
+        return combined_data.sort_values(by="overall", ascending=False)
+    else:
+        return combined_data
--- analysis/scripts/eda.py
+++ analysis/scripts/eda.py
@@ -8,27 +8,37 @@ sys.path.append(project_root)
 
 import pandas as pd
 from analysis.utils.constants import stats_2020_path, stats_2021_path
-from analysis.player_rating import get_players_pool, get_player_ratings
+from analysis.player_rating import (
+    get_players_pool,
+    get_player_ratings,
+    combine_player_data,
+)
 
 stats_2021 = pd.read_csv(stats_2021_path).dropna()
 stats_2020 = pd.read_csv(stats_2020_path).dropna()
 
-data = stats_2020
 players_pool = get_players_pool(n_teams=13, n_players=13, over_write=400)
-player_ratings = get_player_ratings(data, players_pool=players_pool)
+ratings_2021 = get_player_ratings(stats_2021, players_pool=players_pool)
+ratings_2020 = get_player_ratings(stats_2020, players_pool=players_pool)
+player_ratings = combine_player_data(data_2020=ratings_2020, data_2021=ratings_2021)
 
-#%%
-
-
+player_stats = combine_player_data(
+    data_2020=stats_2020.set_index("name"), data_2021=stats_2021.set_index("name")
+)
 
 # %%
+from analysis.player_rating import get_expected_salary, get_similar_players
 
-# * 1 - autocomplete input
-# * 2 - percentage baseline slider
-# * 3 - player pool size slider
-# * 4 - stats dropdown (2020 or 2021)
-# * 5 - category for color dropdown (PTS, STL etc)
-# * 6 - add player name label
-# * 7 - dataframe with selection
+name = "James Harden"
+name = "Jayson Tatum"
+name = "Chris Paul"
+name = "Clint Capela"
+get_similar_players(name, player_ratings=player_ratings, score_method="euclidean")
+
+
+#%%
+salary = get_expected_salary(player_ratings)
+salary.head(20)
+
 
 # %%
